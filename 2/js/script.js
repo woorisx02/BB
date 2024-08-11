3fd053bd50779eee36f248b5016c6916dcedea1b
woorisx02 <162644446+woorisx02@users.noreply.github.com>
2024-08-11 16:26:50 +0900

    Add files via upload

--- 2/js/script.js
+++ 2/js/script.js
@@ -6,21 +6,25 @@ const bodyObj = document.querySelector(".container");
 let topBtn = document.querySelector('.top-btn');
 const loginBtn = document.querySelector('.login-btn');
 const joinBtn = document.querySelector('.join-btn');
-const box = document.querySelector('.box');
+const box = document.querySelector('form');
 const loginBox = document.querySelector('.login-box');
 const joinBox = document.querySelector('.join-box');
 const boxLoginBtn = document.querySelector('button.box-btn-login');
 const boxJoinBtn = document.querySelector('button.box-btn-join');
-// const header = document.querySelector('.container > header');
+
 const headPrivate =document.querySelector('.head-private');
 const nav = document.querySelector('nav');
 const section = document.querySelectorAll('section');
 const article = document.querySelectorAll('article')
 const menuIntro = document.querySelector('#joinus');
 const menuInfo = document.querySelector('#info');
-
-
-
+const subMenu = document.querySelectorAll('.sub-menu');
+const mainMenu = document.querySelectorAll('.main-menu ');
+const subItem_1 = document.querySelector('.main-menu > li > a > i:nth-of-type(1)');
+const subItem_2 = document.querySelector('.main-menu > li > a > i:nth-of-type(2)');
+const menuBg = document.querySelector('.menu-bg');
+const bgColor = document.querySelector('.bg-color');
+const subMenuLi = document.querySelectorAll('.main-menu > li > ul > li > a');
 
 
 
@@ -44,7 +48,28 @@ bodyObj.addEventListener("mousewheel", function (e) {
 { passive: false }
 );
 
+
 const buttonEvent = () =>{
+  subItem_1.addEventListener("click", function(e){
+    e.preventDefault();
+    subItem_1.style.display='none';
+    subItem_2.style.display='block';
+    for(let i =0; i<subMenu.length;i++){
+      subMenu[i].classList.toggle('sub-menu');
+    }
+    menuBg.classList.toggle('menu-bg');
+   
+    
+  });
+  subItem_2.addEventListener("click", function(e){
+    e.preventDefault();
+    subItem_1.style.display='block';
+    subItem_2.style.display='none';
+    for(let i =0; i<subMenu.length;i++){
+      subMenu[i].classList.toggle('sub-menu');
+    }
+    menuBg.classList.toggle('menu-bg');
+  });
   loginBtn.addEventListener("click", function(e){
     e.preventDefault();
     location.href="#main";
@@ -85,13 +110,23 @@ const boxDisplayNone = () =>{
           topBtn.style.display="block";
           headPrivate.style.display="none";
           nav.classList.add("sticky");
+          nav.classList.add("background");
+          for(let i =0; i <subMenuLi.length;i++) {
+            subMenuLi[i].classList.add("background");
+          }
 
+          bgColor.classList.add("background");
         } else {
           topBtn.style.display="none";
-          nav.classList.remove("sticky");
           headPrivate.style.display="block";
+          nav.classList.remove("sticky");
+          nav.classList.remove("background");
+          for(let i =0; i <subMenuLi.length;i++) {
+            subMenuLi[i].classList.remove("background");
+          }
+          bgColor.classList.remove("background");
         }
-
+       
     });
     
     topBtn.addEventListener("click", function() {
@@ -108,7 +143,6 @@ const init = () => {
     window.addEventListener("beforeunload", scrollPageEvent);
     scrollPageEvent();
     boxDisplayNone();
-   
   };
 const scrollPageEvent = () => {
  
