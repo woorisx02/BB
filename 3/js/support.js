b953c98bca22aae62db62a54dc0ef3f2de0e5de7
woorisx02 <162644446+woorisx02@users.noreply.github.com>
2024-09-08 08:29:15 +0900

    Delete 3 directory

--- 3/js/support.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-    $('#trigger').click(function(){
-        $(this).toggleClass('active');
-        $('#main-menu-bg').toggleClass('active');
-        $('#main-menu').toggleClass('active');
-    });
-});
