4fe3adee8d4154bf3b22c05a09b6e89331cfa365
woorisx02 <162644446+woorisx02@users.noreply.github.com>
2024-09-08 08:35:52 +0900

    Add files via upload

---
+++ 4/info.php
@@ -0,0 +1,235 @@
+<? include "include/header.php" ?>
+
+<link rel="stylesheet" href="css/info.css">
+    <div class="bn bn-info-bg">
+		<h2>Book Info</h2>
+	</div>
+    
+    <section>
+		<h3>Recommended <span>book</span> of the <span>month</span></h3>
+        <p class="info-st">이달의 추천도서</p>
+
+		<div class="info-search-wrap">
+			<input type="search" name="search" id="info-search">
+			<i class="bi bi-search"></i>
+		</div>
+
+		<ul class="info-container flex-between">
+			<li>
+				<div class="info-book info-book-bg-1"></div>
+				<div class="info-book-title">
+					<h4>The Money Book</h4>
+					<a href="#"><b><i class="bi bi-box-arrow-up-right"></i></b></a>
+				</div>
+				<p class="info-book-p">출발선이 달라도 금융생활은 평등하도록,
+					잘 살아가기 위해 꼭 알아야 할 100가지...</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-2"></div>
+				<div class="info-book-title">
+					<h4>당신이 누군가를...</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">히가시노 게이고가 재현한
+					황금시대 본격 미스터리</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-3"></div>
+				<div class="info-book-title">
+					<h4>Same as Ever</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">Want to understand the changing 
+					world?
+					Start with what stays the...</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-4"></div>
+				<div class="info-book-title">
+					<h4>만일 나에게 단 ...</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">“당신은 어떻게 살아갈지 선택할 수 있다”
+					내 삶의 가치를 찾는 여섯 번의 인생 수업</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-5"></div>
+				<div class="info-book-title">
+					<h4>Toeic volume 4</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">출제기관 ETS 토익 정기시험 
+					기출문제 10회
+					최신판 Vol.4 독점공개!
+					</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-6"></div>
+				<div class="info-book-title">
+					<h4>호감의 시작</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">다음에 또 만나고 싶은 사람은 
+					무엇이 다를까?</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-7"></div>
+				<div class="info-book-title">
+					<h4>모순</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">인생은 살아가면서 탐구하는 것!</p>
+			</li>
+			<li>
+				<div class="info-book info-book-bg-8"></div>
+				<div class="info-book-title">
+					<h4>허송세월</h4>
+					<a href="#"><i class="bi bi-box-arrow-up-right"></i></a>
+				</div>
+				<p class="info-book-p">“중생의 어리석음은 한이 없는데,
+					나는 이 어리석음과 더불어 편안해지려 한다”</p>
+			</li>
+
+		</ul>
+		
+		<h3 class="info-tt"><span>The</span> new work <span>of</span> the month</h3>
+        <p class="info-st">금년 인기신작</p>
+    </section>
+	<div class="slider">
+		<div class="slides slide-track">
+			<img src="imgs/info/slide-track1.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track2.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track3.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track4.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track5.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track6.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track7.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track1.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track2.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track3.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track4.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track5.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track6.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track7.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track1.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track2.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track3.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track4.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track5.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track6.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track7.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track1.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track2.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track3.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track4.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track5.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track6.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track7.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track1.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track2.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track3.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track4.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track5.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track6.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track7.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track1.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track2.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track3.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track4.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track5.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track6.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track7.jpg" height="150" width="100" alt="">
+		</div>
+
+		<div class="slides slide-track-2">
+			<img src="imgs/info/slide-track8.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track9.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track10.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track11.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track12.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track13.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track14.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track8.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track9.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track10.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track11.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track12.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track13.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track14.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track8.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track9.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track10.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track11.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track12.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track13.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track14.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track8.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track9.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track10.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track11.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track12.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track13.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track14.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track8.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track9.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track10.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track11.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track12.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track13.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track14.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track8.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track9.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track10.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track11.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track12.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track13.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track14.jpg" height="150" width="100" alt="">
+		</div>
+
+		<div class="slides slide-track-3">
+			<img src="imgs/info/slide-track15.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track16.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track17.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track18.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track19.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track20.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track21.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track15.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track16.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track17.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track18.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track19.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track20.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track21.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track15.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track16.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track17.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track18.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track19.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track20.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track21.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track15.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track16.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track17.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track18.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track19.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track20.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track21.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track15.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track16.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track17.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track18.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track19.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track20.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track21.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track15.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track16.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track17.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track18.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track19.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track20.jpg" height="150" width="100" alt="">
+			<img src="imgs/info/slide-track21.jpg" height="150" width="100" alt="">
+		</div>
+	</div>
+
+    <? include "include/footer.php" ?>
